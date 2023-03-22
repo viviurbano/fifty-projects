0b4d830e2fc1aa164c5c0877c96166f3dd72655d
viviurbano <viviane.urbano1@gmail.com>
2023-03-21 21:48:20 -0400

    project 6
    updating folders organization

---
+++ projects/project-05/script.js
@@ -0,0 +1,20 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+let int = setInterval(blurring, 20);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.opacity = scale(load, 0, 100, 0, 1);
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
